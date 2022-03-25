8739346417823f5e4d0b1c1a024bd4273bb329e4
Thijmen <mail@thijmenheuvelink.nl>
2022-03-25 10:49:46 +0100

    update

--- components/NavBar.jsx
+++
@@ -1,8 +0,0 @@
-export default function NavBar() {
-  return (
-    <div className="text-center my-10">
-      <b>Thijmen Heuvelink</b>
-      <p>~ 2022 ~</p>
-    </div>
-  )
-}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,29 @@
-import NavBar from '/components/NavBar'
+import { VscGithub } from 'react-icons/vsc'
 
 export default function Index() {
   return (
-    <>
-      <NavBar />
-    </>
+    <div className="text-center my-10">
+      <b className="my-10">Thijmen Heuvelink</b>
+
+      <div className="my-10">
+        <a href="mailto:mail@thijmenheuvelink.nl"
+          className="transition duration-500 border-2 border-black rounded mx-2 py-2 px-4 hover:bg-black hover:text-white hover:border-none"
+        >contact</a>
+
+        <a href="mailto:support@thijmenheuvelink.nl"
+          className="transition duration-500 border-2 border-black rounded mx-2 py-2 px-4 hover:bg-black hover:text-white hover:border-none"
+        >support</a>
+
+        <a href="https://github.com/ThijmenGThN"
+          className="transition duration-500 border-2 border-black rounded mx-2 py-2 px-4 hover:bg-black hover:text-white hover:border-none"
+        >
+          <button>
+            <VscGithub />
+          </button>
+        </a>
+      </div>
+
+      <p className="my-10">~ 2022 ~</p>
+    </div>
   )
 }
