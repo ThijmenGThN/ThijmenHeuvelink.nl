49d4a9c2fa5c6fab8db2b2696c7b2d86ce07d350
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2023-01-24 15:46:40 +0100

    ✨ Splash

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,27 @@
+import Image from 'next/image'
+
 import type Directus from '@/types/Directus'
 
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import HiThere from '@/resources/assets/hi-there.png'
 
 export default function Index({ API }: { API: Directus }) {
 
   return (
     <div className='flex flex-col min-h-screen'>
       <Navbar />
+
+      <div className='mx-auto container mt-28 justify-center flex'>
+        <div className='max-w-[512px]'>
+          <Image src={HiThere} alt="banner" width="512" height="512"></Image>
+          <p className='font-semibold mx-6 mb-28'>
+            As you have noticed, this website is currently actively being built.
+
+            It's just a matter of time until something amazing happens, keep an eye out for any cool changes!
+          </p>
+        </div>
+      </div>
 
       <div className='grow' />
 
