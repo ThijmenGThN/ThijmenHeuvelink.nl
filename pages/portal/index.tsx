2d543a7f833ba688aa82fe59dbc5b60f76ded10f
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2023-01-24 15:47:07 +0100

    ⚡️Portal redirector

--- pages/portal/index.tsx
+++ pages/portal/index.tsx
@@ -1,19 +1,13 @@
-import { FiLogOut } from 'react-icons/fi'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 import type Directus from '@/types/Directus'
 
 import Navbar from '@/components/Navbar'
-import User from '@/components/portal/User'
-import Switcher from '@/components/portal/Switcher'
 
 export default function Index({ API }: { API: Directus }) {
-  const [me, setMe] = useState()
-
   useEffect(() => {
     API.users.me.read()
-      .then((data: any) => setMe(data))
+      .then(() => window.location.replace('/portal/credentials/'))
       .catch(() => window.location.replace('/portal/login'))
   }, [API])
 
@@ -21,15 +15,10 @@ export default function Index({ API }: { API: Directus }) {
     <>
       <Navbar />
 
-      <div className='mt-16 mx-auto container'>
-        <User me={me} />
-
-        <div className='flex mt-4 gap-2'>
-          <Switcher link='' />
-
-          <div className='grow rounded-lg bg-white w-full border px-4 py-2 flex gap-2'>
-            abc
-          </div>
+      <div className='min-h-screen flex flex-col justify-center items-center'>
+        <div>
+          <p className='text-xl font-bold'>You are being redirected,</p>
+          <p className='text-sm'>this will take less than a jiffy.</p>
         </div>
       </div>
     </>
