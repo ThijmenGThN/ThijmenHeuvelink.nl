5c610418c39645cac322497c26530b5ad42e511f
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2023-04-29 15:05:15 +0200

    🔥 Back 2 Basics

--- source/resources/utils/array.ts
+++
@@ -1,6 +0,0 @@
-
-// Source: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-export const shuffle = (array: Array<any>) =>
-    array.map(value => ({ value, sort: Math.random() }))
-        .sort((a, b) => a.sort - b.sort)
-        .map(({ value }) => value)
